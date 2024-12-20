d96f5024df4283c8fdbd65c5e5e22b4423cc38bc
hjkim27 <hjkim27.dev@gmail.com>
2024-12-21 00:55:07 +0900

    [ADD] #2 restAPIUtil 추가
    - post방식 추가

--- src/main/java/com/hjkim27/util/http/RestApiUtil.java
+++ src/main/java/com/hjkim27/util/http/RestApiUtil.java
@@ -6,8 +6,12 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -93,11 +97,11 @@ public class RestApiUtil {
      * @return
      */
     public static CloseableHttpResponse runGet(
-            String url,
-            Map<String, String> headerMap,
-            Map<String, Object> paramMap,
-            RequestConfig requestConfig,
-            boolean useConnectionManager
+            String url
+            , Map<String, String> headerMap
+            , Map<String, Object> paramMap
+            , RequestConfig requestConfig
+            , boolean useConnectionManager
     ) {
         CloseableHttpClient client = null;
         CloseableHttpResponse response = null;
@@ -145,6 +149,111 @@ public class RestApiUtil {
         } catch (KeyStoreException e) {
             throw new RuntimeException(e);
         } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        } finally {
+        }
+        return response;
+    }
+
+    /**
+     * <pre>
+     *     REST API POST :: MULTIPART_FORM_DATA
+     *     - requestConfig 세팅 추가
+     * </pre>
+     *
+     * @param url
+     * @param headerMap     header info Map
+     * @param multipartMode HttpMultipartMode
+     * @param binaryMap     image byte Map
+     * @param paramMap      parameter Map
+     * @return
+     */
+    public static CloseableHttpResponse runPost(
+            String url
+            , Map<String, String> headerMap
+            , HttpMultipartMode multipartMode
+            , Map<String, byte[]> binaryMap
+            , Map<String, Object> paramMap
+    ) {
+        return runPost(url, headerMap, multipartMode, binaryMap, paramMap, null, false);
+    }
+
+    /**
+     * <pre>
+     *     REST API POST :: MULTIPART_FORM_DATA
+     *     - requestConfig 세팅 추가
+     * </pre>
+     *
+     * @param url
+     * @param headerMap            header info Map
+     * @param multipartMode        HttpMultipartMode
+     * @param binaryMap            image byte Map
+     * @param paramMap             parameter Map
+     * @param requestConfig        requestConfig
+     * @param useConnectionManager PoolingHttpClientConnectionManager 사용여부
+     * @return
+     */
+    public static CloseableHttpResponse runPost(
+            String url
+            , Map<String, String> headerMap
+            , HttpMultipartMode multipartMode
+            , Map<String, byte[]> binaryMap
+            , Map<String, Object> paramMap
+            , RequestConfig requestConfig
+            , boolean useConnectionManager
+    ) {
+        CloseableHttpClient client = null;
+        CloseableHttpResponse response = null;
+        try {
+            client = getClient(url, useConnectionManager);
+            HttpPost post = new HttpPost(url);
+
+            // header
+            if (headerMap != null) {
+                for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    post.setHeader(key, value);
+                }
+            }
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+            // HttpMultipartMode
+            if (multipartMode != null) {
+                builder.setMode(multipartMode);
+            }
+
+            // binaryBody
+            if (binaryMap != null) {
+                for (Map.Entry<String, byte[]> entry : binaryMap.entrySet()) {
+                    builder.addBinaryBody(entry.getKey(), entry.getValue(), ContentType.DEFAULT_BINARY, entry.getKey() + ".jpg");
+                }
+            }
+
+            // parameters
+            if (paramMap != null) {
+                for (Map.Entry<String, Object> entry : paramMap.entrySet()) {
+                    String key = entry.getKey();
+                    Object value = entry.getValue();
+                    builder.addTextBody(key, String.valueOf(value), ContentType.MULTIPART_FORM_DATA);
+                }
+            }
+
+            // requestConfig
+            if (requestConfig != null) {
+                post.setConfig(requestConfig);
+            }
+
+            post.setEntity(builder.build());
+
+            response = client.execute(post);
+            log.info("{} ## HttpStatus {}", url, response.getStatusLine().getStatusCode());
+        } catch (ClientProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
         }
